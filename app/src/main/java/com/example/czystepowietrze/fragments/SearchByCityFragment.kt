fda855d7de1a9f215e53ef2ade0aca83306ab8d1
Patryk Zimny <patryk.zim98@gmail.com>
2021-11-27 13:23:57 +0100

    Dynamic search station in 'SearchByCityFragment' added

--- app/src/main/java/com/example/czystepowietrze/fragments/SearchByCityFragment.kt
+++ app/src/main/java/com/example/czystepowietrze/fragments/SearchByCityFragment.kt
@@ -1,10 +1,13 @@
 package com.example.czystepowietrze.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -37,25 +40,34 @@ class SearchByCityFragment : Fragment() {
         _binding = FragmentSearchByCityBinding.inflate(inflater, container, false)
         val arguments = this.arguments
 
+        val retrofitRepository = RetrofitRepository()
+        val viewModelFactory = FragmentViewModelFactory(retrofitRepository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(FragmentViewModel::class.java)
 
-        binding.buttonSearchByCity.setOnClickListener{
-            if(binding.editTextCity.text.isNotEmpty()){
-                val retrofitRepository = RetrofitRepository()
-                val viewModelFactory = FragmentViewModelFactory(retrofitRepository)
-                viewModel = ViewModelProvider(this, viewModelFactory).get(FragmentViewModel::class.java)
-                viewModel.getAllStations()
-                viewModel.myResponse.observe(viewLifecycleOwner, Observer {response ->
-                    if(response.isSuccessful){
-                        response.body()?.let {
-                            rvAdapter.setData(it, binding.editTextCity.text.toString().lowercase())
-                        }
-                        setupRecyclerview()
-                    }else{
-                        Toast.makeText(context, response.code().toString(), Toast.LENGTH_SHORT).show()
-                    }
-                })
+        binding.editTextCity.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-        }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(binding.editTextCity.text.isNotEmpty()){
+                    viewModel.getAllStations()
+                    viewModel.myResponse.observe(viewLifecycleOwner, Observer {response ->
+                        if(response.isSuccessful){
+                            response.body()?.let {
+                                rvAdapter.setData(it, binding.editTextCity.text.toString().lowercase())
+                            }
+                            setupRecyclerview()
+                        }else{
+                            Toast.makeText(context, response.code().toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
+            }
+
+        })
 
         return binding.root
     }
